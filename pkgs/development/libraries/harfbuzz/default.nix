jamagin/nixpkgs

commit a0ed289f7b2e8e1cec37afeec74c12d6c266d1d3

Replaced ad-hoc gettext buildinput by libintlOrEmpty

pkgs/development/libraries/harfbuzz/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, glib, freetype, gettext }:
+{ stdenv, fetchurl, pkgconfig, glib, freetype, libintlOrEmpty }:
 
 stdenv.mkDerivation rec {
   name = "harfbuzz-0.9.12";
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "19cx5y2m20rp7z5j7mwqfb4ph2g8lrri69zim44x362y4w5gfly6";
   };
 
-  buildInputs = [ pkgconfig glib freetype ] ++ stdenv.lib.optional (!stdenv.isLinux) gettext;
+  buildInputs = [ pkgconfig glib freetype libintlOrEmpty ];
 
   meta = {
     description = "An OpenType text shaping engine";

commit c315301757ef85c7b86ed5ac841d8467b44eaa68

Fix the hash changes accidentally caused in a0ed289f
Moreover I'm not sure It's legal to add *lists inside* buildInputs.

pkgs/development/libraries/harfbuzz/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "19cx5y2m20rp7z5j7mwqfb4ph2g8lrri69zim44x362y4w5gfly6";
   };
 
-  buildInputs = [ pkgconfig glib freetype libintlOrEmpty ];
+  buildInputs = [ pkgconfig glib freetype ] ++ libintlOrEmpty;
 
   meta = {
     description = "An OpenType text shaping engine";