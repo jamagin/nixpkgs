jamagin/nixpkgs

commit 4ab4cea69acdf64805547fdb49273ee00537759c

pkgs/build-support/cabal: set LANG="en_US.UTF-8" for the Haddock run
Haskell packages that contain non-ascii characters in their .cabal file
or somewhere else in their haddock documentation fail to compile under
nixpkgs and usually flagged with noHaddock = true. I wanted to do the
same for modularArithmentic, when I realized that we just have to set
the locale to some UTF-8 compatible locale in build-support/cabal to fix
this issue correctly.

pkgs/build-support/cabal/default.nix
@@ -147,7 +147,11 @@ assert enableCheckPhase -> stdenv.lib.versionOlder "7" ghc.ghcVersion;
               ./Setup build
 
               export GHC_PACKAGE_PATH=$(${ghc.GHCPackages})
-              [ -n "$noHaddock" ] || ./Setup haddock
+              if [ -n "$noHaddock" ]; then
+                export LANG="en_US.UTF-8"
+                ./Setup haddock
+                unset LANG
+              fi
 
               eval "$postBuild"
             '';

commit 6dec62f0e4572ef4cf94356e57deb63ecaffceee

pkgs/build-support/cabal: fix the logic of the 'noHaddock' flag that was broken in 4ab4cea69acdf64805547fdb49273ee00537759c

pkgs/build-support/cabal/default.nix
@@ -150,7 +150,7 @@ assert enableCheckPhase -> stdenv.lib.versionOlder "7" ghc.ghcVersion;
               ./Setup build
 
               export GHC_PACKAGE_PATH=$(${ghc.GHCPackages})
-              if [ -n "$noHaddock" ]; then
+              if [ -z "$noHaddock" ]; then
                 export LANG="en_US.UTF-8"
                 ./Setup haddock
                 unset LANG