jamagin/nixpkgs

commit e2e9678a8e1ef36ebe8f05854493f2464527c908

udisks1: bump to fix CVE-2014-0004
Also systemd unit is now installed.
Thanks to nixpkgs monitor again, as for all my CVE commits.

pkgs/os-specific/linux/udisks/1-default.nix
@@ -3,11 +3,11 @@
 , libxslt, docbook_xsl, utillinux }:
 
 stdenv.mkDerivation rec {
-  name = "udisks-1.0.4";
+  name = "udisks-1.0.5";
 
   src = fetchurl {
     url = "http://hal.freedesktop.org/releases/${name}.tar.gz";
-    sha256 = "1xgqifddwaavmjc8c30i0mdffyirsld7c6qhfyjw7f9khwv8jjw5";
+    sha256 = "0wbg3jrv8limdgvcygf4dqin3y6d30y9pcmmk711vq571vmq5v7j";
   };
 
   patches = [ ./purity.patch ./no-pci-db.patch ];
@@ -28,7 +28,11 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkgconfig ];
 
-  configureFlags = "--localstatedir=/var --enable-lvm2";
+  configureFlags = [
+    "--localstatedir=/var"
+    "--enable-lvm2"
+    "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
+  ];
 
   meta = {
     homepage = http://www.freedesktop.org/wiki/Software/udisks;

commit a60d3bac5400f00017771ab8ee24d98a99eeae2f

Fix services.udisks.enable.
Latest update to udisks in e2e9678a broke it for me. Fix it by doing the
following:

- Add udisks.service to /etc/systemd/system (via systemd.packages)
- Fix path to udisks-daemon in udisks.service (libexec/ instead of lib/)

nixos/modules/services/hardware/udisks.nix
@@ -40,6 +40,8 @@ with pkgs.lib;
       '';
 
     services.udev.packages = [ pkgs.udisks ];
+
+    systemd.packages = [ pkgs.udisks ];
   };
 
 }

pkgs/os-specific/linux/udisks/1-default.nix
@@ -19,6 +19,11 @@ stdenv.mkDerivation rec {
       substituteInPlace src/main.c --replace \
         "/sbin:/bin:/usr/sbin:/usr/bin" \
         "${utillinux}/bin:${mdadm}/sbin:/var/run/current-system/sw/bin:/var/run/current-system/sw/sbin"
+
+      # For some reason @libexec@ is set to 'lib/' when building.
+      # Passing --libexecdir in configureFlags didn't help.
+      substituteInPlace data/systemd/udisks.service.in \
+        --replace "@libexecdir@" "$out/libexec"
     '';
 
   buildInputs =