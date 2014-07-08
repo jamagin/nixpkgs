jamagin/nixpkgs

commit 717ae8162f47ec3ad2239b142c3da3e6febaeab0

texinfo: move ncurses support into texinfoInteractive
This, for example, drops the stdenv dependency on ncurses.

pkgs/development/tools/misc/texinfo/5.2.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, ncurses, perl, xz }:
+{ stdenv, fetchurl, ncurses, perl, xz, interactive ? false }:
 
 stdenv.mkDerivation rec {
   name = "texinfo-5.2";
@@ -8,7 +8,8 @@ stdenv.mkDerivation rec {
     sha256 = "1njfwh2z34r2c4r0iqa7v24wmjzvsfyz4vplzry8ln3479lfywal";
   };
 
-  buildInputs = [ ncurses perl xz ];
+  buildInputs = [ perl xz ]
+    ++ stdenv.lib.optional interactive ncurses;
 
   preInstall = ''
     installFlags="TEXMF=$out/texmf-dist";

pkgs/top-level/all-packages.nix
@@ -3914,6 +3914,9 @@ let
   texinfo5 = callPackage ../development/tools/misc/texinfo/5.2.nix { };
   texinfo4 = texinfo413;
   texinfo = texinfo5;
+  texinfoInteractive = appendToName "interactive" (
+    texinfo.override { interactive = true; }
+  );
 
   texi2html = callPackage ../development/tools/misc/texi2html { };
 

commit b4c8f65d198e24b69f9b7218ff1132eb87149f74

Fix info command
The "info" command has been broken on NixOS since
717ae8162f47ec3ad2239b142c3da3e6febaeab0 (proving that nobody uses
info).

nixos/modules/programs/info.nix
@@ -2,6 +2,8 @@
 
 let
 
+  texinfo = pkgs.texinfoInteractive;
+
   # Quick hack to make the `info' command work properly.  `info' needs
   # a "dir" file containing all the installed Info files, which we
   # don't have (it would be impure to have a package installation
@@ -22,15 +24,15 @@ let
 
       for i in $(IFS=:; echo $INFOPATH); do
           for j in $i/*.info; do
-              ${pkgs.texinfo}/bin/install-info --quiet $j $dir/dir
+              ${texinfo}/bin/install-info --quiet $j $dir/dir
           done
       done
 
-      INFOPATH=$dir:$INFOPATH ${pkgs.texinfo}/bin/info "$@"
+      INFOPATH=$dir:$INFOPATH ${texinfo}/bin/info "$@"
     ''; # */
 
 in
 
 {
-  environment.systemPackages = [ infoWrapper pkgs.texinfo ];
+  environment.systemPackages = [ infoWrapper texinfo ];
 }

pkgs/development/tools/misc/texinfo/5.2.nix
@@ -22,6 +22,7 @@ stdenv.mkDerivation rec {
     homepage = "http://www.gnu.org/software/texinfo/";
     description = "GNU Texinfo, the GNU documentation system";
     license = stdenv.lib.licenses.gpl3Plus;
+    platforms = stdenv.lib.platforms.all;
 
     longDescription = ''
       Texinfo is the official documentation format of the GNU project.

pkgs/top-level/release.nix
@@ -295,7 +295,6 @@ let
       texLive = linux;
       texLiveBeamer = linux;
       texLiveExtra = linux;
-      texinfo = all;
       tightvnc = linux;
       time = linux;
       tinycc = linux;