jamagin/nixpkgs

commit 9d09ae6ba393e325263d5d80ab7ae85294696cd2

python: fix argv[0] for wrapped console_scripts.
Previously programs would see '.<program>-wrapped' as their program name. This is a workaround
but should be reasonably safe and reliable.

pkgs/development/python-modules/generic/wrap.sh
@@ -26,6 +26,7 @@ wrapPythonProgramsIn() {
             # dont wrap EGG-INFO scripts since they are called from python
             if echo "$i" | grep -v EGG-INFO/scripts; then
                 echo "wrapping \`$i'..."
+                sed -i "$i" -e "/^#\!/a import sys; sys.argv[0] = '$(basename $i)'"
                 wrapProgram "$i" \
                     --prefix PYTHONPATH ":" $program_PYTHONPATH \
                     --prefix PATH ":" $program_PATH

commit aa7de14425792b68bd77948d2fa678d2a72a93ac

python-wrapper: Fix fixup for wrapped argv[0].
Fixes issues introduced by 9d09ae6ba393e325263d5d80ab7ae85294696cd2.

The problem here is that "import sys; sys.argv[0] = ..." is just
appended after the first "#!", which in turn breaks things such as
encoding specifications. A second problem - although not very common -
is when there's another #! within the script.

This should take care of both cases.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/development/python-modules/generic/wrap.sh
@@ -26,7 +26,9 @@ wrapPythonProgramsIn() {
             # dont wrap EGG-INFO scripts since they are called from python
             if echo "$i" | grep -v EGG-INFO/scripts; then
                 echo "wrapping \`$i'..."
-                sed -i "$i" -e "/^#\!/a import sys; sys.argv[0] = '$(basename $i)'"
+                sed -i "$i" -e '1,/#!/ { /^#\!/,/^[^#]/ {
+                    /^[^#]/i import sys; sys.argv[0] = '"'$(basename "$i")'"'
+                }}'
                 wrapProgram "$i" \
                     --prefix PYTHONPATH ":" $program_PYTHONPATH \
                     --prefix PATH ":" $program_PATH