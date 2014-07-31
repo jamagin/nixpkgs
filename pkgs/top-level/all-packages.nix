jamagin/nixpkgs

commit bb359c13ea027cb55592cafe07c8449160a7943a

lttng: align attribute name with package name
lttngModules => lttng-modules
lttngTools => lttng-tools
lttngUst => lttng-ust

But keep the old attributes around for backward compatibility.

pkgs/top-level/all-packages.nix
@@ -4006,9 +4006,9 @@ let
 
   ltrace = callPackage ../development/tools/misc/ltrace { };
 
-  lttngTools = callPackage ../development/tools/misc/lttng-tools { };
+  lttng-tools = callPackage ../development/tools/misc/lttng-tools { };
 
-  lttngUst = callPackage ../development/tools/misc/lttng-ust { };
+  lttng-ust = callPackage ../development/tools/misc/lttng-ust { };
 
   lttv = callPackage ../development/tools/misc/lttv { };
 
@@ -7439,7 +7439,7 @@ let
 
     ktap = callPackage ../os-specific/linux/ktap { };
 
-    lttngModules = callPackage ../os-specific/linux/lttng-modules { };
+    lttng-modules = callPackage ../os-specific/linux/lttng-modules { };
 
     broadcom_sta = callPackage ../os-specific/linux/broadcom-sta/default.nix { };
 
@@ -11418,6 +11418,9 @@ let
   # Attributes for backward compatibility.
   adobeReader = adobe-reader;
   asciidocFull = asciidoc-full;  # added 2014-06-22
+  lttngModules = lttng-modules;  # added 2014-07-31
+  lttngTools = lttng-tools;  # added 2014-07-31
+  lttngUst = lttng-ust;  # added 2014-07-31
 
 
 }; in self; in pkgs

pkgs/development/tools/misc/lttng-tools/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, popt, libuuid, liburcu, lttngUst, kmod }:
+{ stdenv, fetchurl, popt, libuuid, liburcu, lttng-ust, kmod }:
 
 stdenv.mkDerivation rec {
   name = "lttng-tools-2.4.1";
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "1v9f7a3c2shwgn4g759bblgr27h9ql9sfq71r1mbkf8rd235g2jr";
   };
 
-  buildInputs = [ popt libuuid liburcu lttngUst ];
+  buildInputs = [ popt libuuid liburcu lttng-ust ];
 
   prePatch = ''
     sed -e "s|/sbin/modprobe|${kmod}/sbin/modprobe|g" \

commit 8dd384fdb63c8937baa43c1983ce7d47917ef266

lttng-modules: fix bad attribute reference
Fixup error introduced in commit
bb359c13ea027cb55592cafe07c8449160a7943a ("lttng: align attribute name
with package name"). lttng-modules is not a top-level attribute.

pkgs/top-level/all-packages.nix
@@ -7440,6 +7440,7 @@ let
     ktap = callPackage ../os-specific/linux/ktap { };
 
     lttng-modules = callPackage ../os-specific/linux/lttng-modules { };
+    lttngModules = lttng-modules;  # added 2014-07-31
 
     broadcom_sta = callPackage ../os-specific/linux/broadcom-sta/default.nix { };
 
@@ -11420,7 +11421,6 @@ let
   # Attributes for backward compatibility.
   adobeReader = adobe-reader;
   asciidocFull = asciidoc-full;  # added 2014-06-22
-  lttngModules = lttng-modules;  # added 2014-07-31
   lttngTools = lttng-tools;  # added 2014-07-31
   lttngUst = lttng-ust;  # added 2014-07-31
 