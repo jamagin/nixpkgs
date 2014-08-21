jamagin/nixpkgs

commit 344cbb30172e8875085fb7ac0fe01ace9a6556db

emacs: On Darwin, don't depend on gconf and cairo

pkgs/applications/editors/emacs-24/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, ncurses, x11, libXaw, libXpm, Xaw3d
 , pkgconfig, gtk, libXft, dbus, libpng, libjpeg, libungif
-, libtiff, librsvg, texinfo, gconf, libxml2, imagemagick, gnutls
+, libtiff, librsvg, texinfo, gconf, libxml2, imagemagick, openssl
 , alsaLib, cairo
 , withX ? !stdenv.isDarwin, withGTK ? true
 }:
@@ -21,12 +21,12 @@ stdenv.mkDerivation rec {
   patches = [ ./darwin-new-sections.patch ];
 
   buildInputs =
-    [ ncurses gconf libxml2 gnutls alsaLib pkgconfig texinfo ]
+    [ ncurses gconf libxml2 openssl alsaLib pkgconfig texinfo ]
     ++ stdenv.lib.optional stdenv.isLinux dbus
     ++ stdenv.lib.optionals withX
       [ x11 libXaw Xaw3d libXpm libpng libjpeg libungif libtiff librsvg libXft
-        imagemagick gtk ]
-    ++ stdenv.lib.optional stdenv.isDarwin cairo;
+        imagemagick gtk gconf ]
+    ++ stdenv.lib.optional (stdenv.isDarwin && withX) cairo;
 
   configureFlags =
     ( if withX && withGTK then

commit e5780640d4780d6b59d559bf4cb542f08ffaa61c

Emacs: Use gnutls instead of OpenSSL.
It looks like this error was introduced accidentally with 344cbb3. Emacs
can't use openssl to make secure connections.

pkgs/applications/editors/emacs-24/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, ncurses, x11, libXaw, libXpm, Xaw3d
 , pkgconfig, gtk, libXft, dbus, libpng, libjpeg, libungif
-, libtiff, librsvg, texinfo, gconf, libxml2, imagemagick, openssl
+, libtiff, librsvg, texinfo, gconf, libxml2, imagemagick, gnutls
 , alsaLib, cairo
 , withX ? !stdenv.isDarwin, withGTK ? true
 }:
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
   patches = [ ./darwin-new-sections.patch ];
 
   buildInputs =
-    [ ncurses gconf libxml2 openssl alsaLib pkgconfig texinfo ]
+    [ ncurses gconf libxml2 gnutls alsaLib pkgconfig texinfo ]
     ++ stdenv.lib.optional stdenv.isLinux dbus
     ++ stdenv.lib.optionals withX
       [ x11 libXaw Xaw3d libXpm libpng libjpeg libungif libtiff librsvg libXft