jamagin/nixpkgs

commit bc86b78d8cb469507dbb324a92d019f1efec3098

update http-client

pkgs/development/libraries/haskell/http-client/default.nix
@@ -9,8 +9,8 @@
 
 cabal.mkDerivation (self: {
   pname = "http-client";
-  version = "0.3.8.2";
-  sha256 = "06c34h7wvn3mbr8lrkcmjygj22d94mr5rxzqwpjfdjgimd98hw3s";
+  version = "0.4.2.2";
+  sha256 = "0jcpjvfhxk3vlhy7ndand6zghs2f7mll8w0zp9x9bzfrp3kldqxk";
   buildDepends = [
     base64Bytestring blazeBuilder caseInsensitive cookie
     dataDefaultClass deepseq exceptions filepath httpTypes mimeTypes

commit fbb2fce4e151cd01cef431a43b5ab88ef40a2b44

http-client: revert update to version 0.4.2.2
Commit bc86b78d broke pandoc, which in turn breaks half a dozen other packages.

pkgs/development/libraries/haskell/http-client/default.nix
@@ -9,8 +9,8 @@
 
 cabal.mkDerivation (self: {
   pname = "http-client";
-  version = "0.4.2.2";
-  sha256 = "0jcpjvfhxk3vlhy7ndand6zghs2f7mll8w0zp9x9bzfrp3kldqxk";
+  version = "0.3.8.2";
+  sha256 = "06c34h7wvn3mbr8lrkcmjygj22d94mr5rxzqwpjfdjgimd98hw3s";
   buildDepends = [
     base64Bytestring blazeBuilder caseInsensitive cookie
     dataDefaultClass deepseq exceptions filepath httpTypes mimeTypes