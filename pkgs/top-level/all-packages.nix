jamagin/nixpkgs

commit 3eced64b1d4bfa99a2f8df666771ff454932d582

youtube-dl: rename attrname youtubeDl -> youtube-dl
So that both attrname and pkgname is the same. Keep the old attrname
around for backward compatibility.

pkgs/top-level/all-packages.nix
@@ -2741,7 +2741,7 @@ let
 
   xvfb_run = callPackage ../tools/misc/xvfb-run { inherit (texFunctions) fontsConf; };
 
-  youtubeDL = callPackage ../tools/misc/youtube-dl { };
+  youtube-dl = callPackage ../tools/misc/youtube-dl { };
 
   zbar = callPackage ../tools/graphics/zbar {
     pygtk = lib.overrideDerivation pygtk (x: {
@@ -12420,6 +12420,7 @@ let
   lttngTools = lttng-tools;  # added 2014-07-31
   lttngUst = lttng-ust;  # added 2014-07-31
   jquery_ui = jquery-ui;  # added 2014-09-07
+  youtubeDl = youtube-dl;  # added 2014-10-26
 
 
 }; in self; in pkgs

commit f8aaffc17a18690e8b8ee1c69ad426f2a7cefb92

youtube-dl: fix typo
Introduced in commit 3eced64b1d4bfa99a2f8df666771ff454932d582
("youtube-dl: rename attrname youtubeDl -> youtube-dl"). The old
attribute was spelled with a capital 'L'.

pkgs/top-level/all-packages.nix
@@ -12422,7 +12422,7 @@ let
   lttngTools = lttng-tools;  # added 2014-07-31
   lttngUst = lttng-ust;  # added 2014-07-31
   jquery_ui = jquery-ui;  # added 2014-09-07
-  youtubeDl = youtube-dl;  # added 2014-10-26
+  youtubeDL = youtube-dl;  # added 2014-10-26
 
 
 }; in self; in pkgs