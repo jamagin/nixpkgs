jamagin/nixpkgs

commit 96e810753807a17d6b55046c60ea256de805e505

firefox-wrapper: add missing gstreamer plugins
These GStreamer plugins are required for H264 <video> tag support in Firefox.

pkgs/top-level/all-packages.nix
@@ -11154,6 +11154,7 @@ let
          );
       libs = [ gstreamer gst_plugins_base ] ++ lib.optionals (cfg.enableQuakeLive or false)
              (with xlibs; [ stdenv.gcc libX11 libXxf86dga libXxf86vm libXext libXt alsaLib zlib ]);
+      gst_plugins = [ gst_plugins_base gst_plugins_good gst_ffmpeg ];
       gtk_modules = [ libcanberra ];
     };
 

pkgs/applications/networking/browsers/firefox/wrapper.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, browser, makeDesktopItem, makeWrapper, plugins, libs, gtk_modules
+{ stdenv, lib, browser, makeDesktopItem, makeWrapper, plugins, gst_plugins, libs, gtk_modules
 , browserName, desktopName, nameSuffix, icon
 }:
 
@@ -32,6 +32,7 @@ stdenv.mkDerivation {
         --suffix-each LD_LIBRARY_PATH ':' "$libs" \
         --suffix-each GTK_PATH ':' "$gtk_modules" \
         --suffix-each LD_PRELOAD ':' "$(cat $(filterExisting $(addSuffix /extra-ld-preload $plugins)))" \
+        --suffix-each GST_PLUGIN_PATH ':' "$gst_plugins" \
         --prefix-contents PATH ':' "$(filterExisting $(addSuffix /extra-bin-path $plugins))"
 
     mkdir -p $out/share/applications
@@ -48,6 +49,7 @@ stdenv.mkDerivation {
   # where to find the plugin in its tree.
   plugins = map (x: x + x.mozillaPlugin) plugins;
   libs = map (x: x + "/lib") libs ++ map (x: x + "/lib64") libs;
+  gst_plugins = map (x: x + "/lib/gstreamer-0.10") gst_plugins;
   gtk_modules = map (x: x + x.gtkModule) gtk_modules;
 
   meta = {

commit 8faf8187fddada29fff7f07322193cd092befe48

firefox-wrapper: more gstreamer plugins
The previous commit 96e8107 broke some of soundcloud in firefox.

pkgs/top-level/all-packages.nix
@@ -11181,7 +11181,7 @@ let
          );
       libs = [ gstreamer gst_plugins_base ] ++ lib.optionals (cfg.enableQuakeLive or false)
              (with xlibs; [ stdenv.gcc libX11 libXxf86dga libXxf86vm libXext libXt alsaLib zlib ]);
-      gst_plugins = [ gst_plugins_base gst_plugins_good gst_ffmpeg ];
+      gst_plugins = [ gst_plugins_base gst_plugins_good gst_plugins_bad gst_plugins_ugly gst_ffmpeg ];
       gtk_modules = [ libcanberra ];
     };
 