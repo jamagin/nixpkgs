jamagin/nixpkgs

commit ba0ca9cdc81a1d255bacadca9acec9ff1ba464dd

gyp: svn-1977 -> 2015-05-17

pkgs/top-level/python-packages.nix
@@ -5811,23 +5811,25 @@ let
   };
 
   gyp = buildPythonPackage rec {
-    rev = "1977";
-    name = "gyp-r${rev}";
+    name = "gyp-${version}";
+    version = "2015-05-15";
 
-    src = pkgs.fetchsvn {
-      url = "http://gyp.googlecode.com/svn/trunk";
-      inherit rev;
-      sha256 = "0vnr75yd3bidysiwl9lljvf1dv6v9m9xqdnx0hdgyl92w689n9j8";
+    src = pkgs.fetchgit {
+      url = "https://chromium.googlesource.com/external/gyp.git";
+      rev = "9f594095c5b14f8bc518081a660e77890c294861";
+      sha256 = "1xqi44alnw9c31jg2hz7flz5nabq003b4jyin12h3s9zl82y6vd5";
     };
 
     patches = optionals pkgs.stdenv.isDarwin [
       ../development/python-modules/gyp/no-darwin-cflags.patch
     ];
 
-    meta = {
-      homepage = http://code.google.com/p/gyp;
-      license = stdenv.lib.licenses.bsd3;
-      description = "Generate Your Projects";
+    meta = with stdenv.lib; {
+      description = "A tool to generate native build files";
+      homepage = https://chromium.googlesource.com/external/gyp/+/master/README.md;
+      license = licenses.bsd3;
+      maintainers = with maintainers; [ codyopel ];
+      platforms = platforms.all;
     };
   };
 

commit 04fe7b6338ca21e2ffb2c0a9878d9b71bf66610e

gyp: Revive old SVN version to build Chromium.
Works around regression from ba0ca9cdc81a1d255bacadca9acec9ff1ba464dd.

We're also marking it lowPrio to make sure nobody will accidentally
reference it using nix-env -i.

Until we have fixed #7402, we're going to build with the old gyp version
to prevent being affected by https://crbug.com/462153.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/applications/networking/browsers/chromium/common.nix
@@ -113,7 +113,7 @@ let
       glib gtk dbus_glib
       libXScrnSaver libXcursor libXtst mesa
       pciutils protobuf speechd libXdamage
-      pythonPackages.gyp pythonPackages.ply pythonPackages.jinja2
+      pythonPackages.gyp_svn1977 pythonPackages.ply pythonPackages.jinja2
     ] ++ optional gnomeKeyringSupport libgnome_keyring3
       ++ optionals gnomeSupport [ gnome.GConf libgcrypt ]
       ++ optional enableSELinux libselinux

pkgs/top-level/python-packages.nix
@@ -5833,6 +5833,28 @@ let
     };
   };
 
+  # Needed to build Chromium until #7402 is fixed.
+  gyp_svn1977 = pkgs.lowPrio (buildPythonPackage rec {
+    rev = "1977";
+    name = "gyp-r${rev}";
+
+    src = pkgs.fetchsvn {
+      url = "http://gyp.googlecode.com/svn/trunk";
+      inherit rev;
+      sha256 = "0vnr75yd3bidysiwl9lljvf1dv6v9m9xqdnx0hdgyl92w689n9j8";
+    };
+
+    patches = optionals pkgs.stdenv.isDarwin [
+      ../development/python-modules/gyp/no-darwin-cflags.patch
+    ];
+
+    meta = {
+      homepage = http://code.google.com/p/gyp;
+      license = stdenv.lib.licenses.bsd3;
+      description = "Generate Your Projects";
+    };
+  });
+
   guessit = buildPythonPackage rec {
     version = "0.9.4";
     name = "guessit-${version}";