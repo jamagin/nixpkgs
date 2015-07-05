jamagin/nixpkgs

commit 3d88908539730c34715036b40b4b19cba439b1fc

xdg_utils: fix handling of runtime dependencies, closes #8564
'egrep' was substituted with the path to the grep binary

pkgs/tools/X11/xdg-utils/default.nix
@@ -17,8 +17,8 @@ stdenv.mkDerivation rec {
     for item in $out/bin/*; do
       substituteInPlace $item --replace "cut " "${coreutils}/bin/cut "
       substituteInPlace $item --replace "sed " "${gnused}/bin/sed "
-      substituteInPlace $item --replace "grep " "${gnugrep}/bin/grep "
       substituteInPlace $item --replace "egrep " "${gnugrep}/bin/egrep "
+      sed -i $item -e "s#[^e]grep #${gnugrep}/bin/grep #g" # Don't replace 'egrep'
       substituteInPlace $item --replace "which " "${which}/bin/which "
       substituteInPlace $item --replace "/usr/bin/file" "${file}/bin/file"
     done

commit 21549d65173f7b722ee29eb9ccf6c83f281ce5f7

xdg_utils: Fix wrong substitution of grep command.
Fixes regression introduced by 3d88908539730c34715036b40b4b19cba439b1fc.

Not replacing "egrep" with a negated character class on [^e] needs to be
put back into the replacement, because if we have something like:

foo="$(grep xxx)"

The replacement would be something like this:

foo="$/nix/store/.../bin/grep xxx)"

Which will lead to wrong behavior and in cases of for example
"xdg-screensaver", even directly to a syntax error:

xdg-screensaver: line 178: syntax error near unexpected token `('
xdg-screensaver: line 178: `command="/nix/store/.../bin/grep -E
                            "^Exec(\[[^]=]*])?=" "$file" |
                            /nix/store/.../bin/cut -d= -f 2- |
                            first_word`"'

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/tools/X11/xdg-utils/default.nix
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
       substituteInPlace $item --replace "cut " "${coreutils}/bin/cut "
       substituteInPlace $item --replace "sed " "${gnused}/bin/sed "
       substituteInPlace $item --replace "egrep " "${gnugrep}/bin/egrep "
-      sed -i $item -e "s#[^e]grep #${gnugrep}/bin/grep #g" # Don't replace 'egrep'
+      sed -i $item -re "s#([^e])grep #\1${gnugrep}/bin/grep #g" # Don't replace 'egrep'
       substituteInPlace $item --replace "which " "${which}/bin/which "
       substituteInPlace $item --replace "/usr/bin/file" "${file}/bin/file"
     done