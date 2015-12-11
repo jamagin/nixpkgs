jamagin/nixpkgs

commit 50972fba2a0aad045f8afba359f1d4b5242d3ed5

python pyside: move callPackage to python-packages.nix
This should make pyside available for all Python versions.

pkgs/top-level/all-packages.nix
@@ -9045,15 +9045,15 @@ let
 
   pyqt4 = pythonPackages.pyqt4;
 
-  pysideApiextractor = callPackage ../development/python-modules/pyside/apiextractor.nix { };
+  pysideApiextractor = pythonPackages.pysideApiextractor;
 
-  pysideGeneratorrunner = callPackage ../development/python-modules/pyside/generatorrunner.nix { };
+  pysideGeneratorrunner = pythonPackages.pysideGeneratorrunner;
 
-  pyside = callPackage ../development/python-modules/pyside { };
+  pyside = pythonPackages.pyside;
 
-  pysideTools = callPackage ../development/python-modules/pyside/tools.nix { };
+  pysideTools = pythonPackages.pysideTools;
 
-  pysideShiboken = callPackage ../development/python-modules/pyside/shiboken.nix { };
+  pysideShiboken = pythonpackages.pysideShiboken;
 
   pyxml = callPackage ../development/python-modules/pyxml { };
 

pkgs/development/python-modules/pyside/apiextractor.nix
@@ -1,7 +1,7 @@
-{ stdenv, fetchurl, cmake, libxml2, libxslt, python27Packages, qt4 }:
+{ stdenv, fetchurl, cmake, libxml2, libxslt, python, sphinx, qt4 }:
 
 stdenv.mkDerivation {
-  name = "pyside-apiextractor-0.10.10";
+  name = "${python.libPrefix}-pyside-apiextractor-0.10.10";
 
   src = fetchurl {
     url = "https://github.com/PySide/Apiextractor/archive/0.10.10.tar.gz";
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
 
   enableParallelBuilding = true;
 
-  buildInputs = [ cmake libxml2 libxslt python27Packages.sphinx qt4 ];
+  buildInputs = [ cmake libxml2 libxslt sphinx qt4 ];
 
   meta = {
     description = "Eases the development of bindings of Qt-based libraries for high level languages by automating most of the process";

pkgs/development/python-modules/pyside/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, cmake, pysideGeneratorrunner, pysideShiboken, qt4 }:
+{ stdenv, fetchurl, cmake, python, pysideGeneratorrunner, pysideShiboken, qt4 }:
 
 stdenv.mkDerivation rec {
   name = "${python.libPrefix}-pyside-${version}";

pkgs/development/python-modules/pyside/generatorrunner.nix
@@ -1,7 +1,7 @@
-{ stdenv, fetchurl, cmake, pysideApiextractor, python27Packages, qt4 }:
+{ stdenv, fetchurl, cmake, pysideApiextractor, python, sphinx, qt4 }:
 
 stdenv.mkDerivation {
-  name = "pyside-generatorrunner-0.6.16";
+  name = "${python.libPrefix}-pyside-generatorrunner-0.6.16";
 
   src = fetchurl {
     url = "https://github.com/PySide/Generatorrunner/archive/0.6.16.tar.gz";
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
 
   enableParallelBuilding = true;
 
-  buildInputs = [ cmake pysideApiextractor python27Packages.sphinx qt4 ];
+  buildInputs = [ cmake pysideApiextractor sphinx qt4 ];
 
   meta = {
     description = "Eases the development of binding generators for C++ and Qt-based libraries by providing a framework to help automating most of the process";

pkgs/development/python-modules/pyside/shiboken.nix
@@ -1,6 +1,7 @@
-{ stdenv, fetchurl, cmake, pysideApiextractor, pysideGeneratorrunner, python27, python27Packages, qt4 }:
+{ stdenv, fetchurl, cmake, libxml2, libxslt, pysideApiextractor, pysideGeneratorrunner, python, sphinx, qt4, isPy3k, isPy35 }:
 
-stdenv.mkDerivation rec {
+# Python 3.5 is not supported: https://github.com/PySide/Shiboken/issues/77
+if isPy35 then throw "shiboken not supported for interpreter ${python.executable}" else stdenv.mkDerivation rec {
   name = "${python.libPrefix}-pyside-shiboken-${version}";
   version = "1.2.4";
 
@@ -11,13 +12,15 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  buildInputs = [ cmake pysideApiextractor pysideGeneratorrunner python27 python27Packages.sphinx qt4 ];
+  buildInputs = [ cmake libxml2 libxslt pysideApiextractor pysideGeneratorrunner python sphinx qt4 ];
 
   preConfigure = ''
     echo "preConfigure: Fixing shiboken_generator install target."
     substituteInPlace generator/CMakeLists.txt --replace \
       \"$\{GENERATORRUNNER_PLUGIN_DIR}\" lib/generatorrunner/
   '';
+
+  cmakeFlags = if isPy3k then "-DUSE_PYTHON3=TRUE" else null;
 
   meta = {
     description = "Plugin (front-end) for pyside-generatorrunner, that generates bindings for C++ libraries using CPython source code";

pkgs/development/python-modules/pyside/tools.nix
@@ -1,7 +1,7 @@
-{ stdenv, fetchurl, cmake, pyside, python27, qt4, pysideShiboken }:
+{ stdenv, fetchurl, cmake, pyside, python, qt4, pysideShiboken }:
 
 stdenv.mkDerivation {
-  name = "pyside-tools-0.2.15";
+  name = "${python.libPrefix}-pyside-tools-0.2.15";
 
   src = fetchurl {
     url = "https://github.com/PySide/Tools/archive/0.2.15.tar.gz";
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
 
   enableParallelBuilding = true;
 
-  buildInputs = [ cmake pyside python27 qt4 pysideShiboken ];
+  buildInputs = [ cmake pyside python qt4 pysideShiboken ];
 
   meta = {
     description = "Tools for pyside, the LGPL-licensed Python bindings for the Qt cross-platform application and UI framework";

pkgs/top-level/python-packages.nix
@@ -169,6 +169,16 @@ in modules // {
     qt5 = pkgs.qt5;
   };
 
+  pyside = callPackage ../development/python-modules/pyside { };
+
+  pysideApiextractor = callPackage ../development/python-modules/pyside/apiextractor.nix { };
+
+  pysideGeneratorrunner = callPackage ../development/python-modules/pyside/generatorrunner.nix { };
+
+  pysideShiboken = callPackage ../development/python-modules/pyside/shiboken.nix { };
+
+  pysideTools = callPackage ../development/python-modules/pyside/tools.nix { };
+
   sip = callPackage ../development/python-modules/sip { };
 
   sip_4_16 = callPackage ../development/python-modules/sip/4.16.nix { };

commit 52ab79d4ce7956c8a5533c39d964e1b32bff88f8

all-packages: Fix typo in "pythonpackages".
Regression introduced by 50972fba2a0aad045f8afba359f1d4b5242d3ed5.

All lower-case won't evaluate very well.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/top-level/all-packages.nix
@@ -9053,7 +9053,7 @@ let
 
   pysideTools = pythonPackages.pysideTools;
 
-  pysideShiboken = pythonpackages.pysideShiboken;
+  pysideShiboken = pythonPackages.pysideShiboken;
 
   pyxml = callPackage ../development/python-modules/pyxml { };
 