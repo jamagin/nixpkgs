jamagin/nixpkgs

commit 195b153d5a0be21471291b82c81550ede8d04b71

openvpn: 2.3.7 -> 2.3.8

pkgs/tools/networking/openvpn/default.nix
@@ -3,11 +3,11 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  name = "openvpn-2.3.7";
+  name = "openvpn-2.3.8";
 
   src = fetchurl {
     url = "http://swupdate.openvpn.net/community/releases/${name}.tar.gz";
-    sha256 = "0vhl0ddpxqfibc0ah0ci7ix9bs0cn5shhmhijg550qpbdb6s80hz";
+    sha256 = "0lbw22qv3m0axhs13razr6b4x1p7jcpvf9rzb15b850wyvpka92k";
   };
 
   patches = optional stdenv.isLinux ./systemd-notify.patch;
@@ -21,6 +21,8 @@ stdenv.mkDerivation rec {
     --enable-systemd
     --enable-iproute2
     IPROUTE=${iproute}/sbin/ip
+  '' + optionalString stdenv.isDarwin ''
+    --disable-plugin-auth-pam
   '';
 
   postInstall = ''

commit ac15adbf8ea48a0f57a8d34dd9666b5f13d5a72d

openvpn: 2.3.8 -> 2.3.10
In particular, this fixes the systemd-ask-password regression
re-introduced by 195b153d5a0be21471291b82c81550ede8d04b71.

pkgs/tools/networking/openvpn/default.nix
@@ -3,11 +3,11 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  name = "openvpn-2.3.8";
+  name = "openvpn-2.3.10";
 
   src = fetchurl {
     url = "http://swupdate.openvpn.net/community/releases/${name}.tar.gz";
-    sha256 = "0lbw22qv3m0axhs13razr6b4x1p7jcpvf9rzb15b850wyvpka92k";
+    sha256 = "1xn8kv4v4h4v8mhd9k4s9rilb7k30jgb9rm7n4fwmfrm5swvbc7q";
   };
 
   patches = optional stdenv.isLinux ./systemd-notify.patch;