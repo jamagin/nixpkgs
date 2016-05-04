jamagin/nixpkgs

commit 635d7178f3fcb0a34e3388843ea3c55555078dad

chromium/updater: Don't import <nixpkgs> again
This effectively resets the attributes given at the point the main
<nixpkgs> is imported and thus for example is also reading in stuff like
~/.nixpkgs/config.nix again, which might lead to unexpected results.

We now only import <nixpkgs> now if the updater is auto-called (like in
update.sh), otherwise the required attributes are passed by callPackage
within the Chromium scope.

I remember noting about this a while ago either on IRC or on GitHub, but
I can't find it right now, so thanks to @obadz for reminding me about
this in #15225.

Tested this by running the updater and also using:

NIXPKGS_CONFIG=$(pwd)/broken.nix nix-instantiate --arg config {} -A chromium

The contents of broken.nix were:

EVALERR{

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Fixes: #15225

pkgs/applications/networking/browsers/chromium/default.nix
@@ -19,9 +19,7 @@ let
   callPackage = newScope chromium;
 
   chromium = {
-    upstream-info = (import ./update.nix {
-      inherit (stdenv) system;
-    }).getChannel channel;
+    upstream-info = (callPackage ./update.nix {}).getChannel channel;
 
     mkChromiumDerivation = callPackage ./common.nix {
       inherit enableSELinux enableNaCl enableHotwording gnomeSupport

pkgs/applications/networking/browsers/chromium/update.nix
@@ -1,9 +1,16 @@
-{ system ? builtins.currentSystem }:
+let maybePkgs = import ../../../../../. {}; in
+
+{ stdenv     ? maybePkgs.stdenv
+, runCommand ? maybePkgs.runCommand
+, fetchurl   ? maybePkgs.fetchurl
+, writeText  ? maybePkgs.writeText
+, curl       ? maybePkgs.curl
+, cacert     ? maybePkgs.cacert
+, nix        ? maybePkgs.nix
+}:
 
 let
-  inherit (import ../../../../../. {
-    inherit system;
-  }) lib runCommand fetchurl writeText stdenv curl cacert nix;
+  inherit (stdenv) lib;
 
   sources = if builtins.pathExists ./upstream-info.nix
             then import ./upstream-info.nix

commit 8b6211d5e4dbed8ffd97ffe664afc139850b40c6

chromium+chrome: Don't import update.nix directly
Regression introduced by 635d7178f3fcb0a34e3388843ea3c55555078dad.

Let's now expose and use the upstream-info attribute via the main
Chromium derivation, so that other packages like the google-chrome
package doesn't need to rely on internals of the Chromium
implementation.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/applications/networking/browsers/chromium/default.nix
@@ -84,6 +84,7 @@ in stdenv.mkDerivation {
   inherit (chromium.browser) meta packageName;
 
   passthru = {
+    inherit (chromium) upstream-info;
     mkDerivation = chromium.mkChromiumDerivation;
   };
 }

pkgs/applications/networking/browsers/google-chrome/default.nix
@@ -26,13 +26,13 @@
 # Necessary for USB audio devices.
 , pulseSupport ? true, libpulseaudio ? null
 
+# Only needed for getting information about upstream binaries
+, chromium
 }:
 
 with stdenv.lib;
 
-with (import ../chromium/update.nix {
-  inherit (stdenv) system;
-}).getChannel channel;
+with chromium.upstream-info;
 
 let
   opusWithCustomModes = libopus.override {