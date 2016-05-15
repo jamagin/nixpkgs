jamagin/nixpkgs

commit f71cba2ff6c866ec819d1bd66472a9044db74512

use dontBuild instead of hacks
changes:
 * buildPhase = "true"
 * buildPhase = ":"

pkgs/data/fonts/dosemu-fonts/default.nix
@@ -9,8 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "0l1zwmw42mpakjrzmbygshcg2qzq9mv8lx42738rz3j9hrqzg4pw";
   };
 
-  configurePhase = "true";
-  buildPhase = "true";
+  dontBuild = true;
 
   buildInputs = [bdftopcf mkfontdir mkfontscale];
 

pkgs/applications/audio/baudline/default.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
   # Prebuilt binary distribution.
   # "patchelf --set-rpath" seems to break the application (cannot start), using
   # LD_LIBRARY_PATH wrapper script instead.
-  buildPhase = "true";
+  dontBuild = true;
   installPhase = ''
     mkdir -p "$out/bin"
     mkdir -p "$out/libexec/baudline"

pkgs/applications/graphics/sane/backends/brscan4/default.nix
@@ -1,12 +1,6 @@
 { stdenv, fetchurl, callPackage, patchelf, makeWrapper, coreutils, libusb }:
 
-/*
-
-
-*/
-
 let
-
   myPatchElf = file: with stdenv.lib; ''
     patchelf --set-interpreter \
       ${stdenv.glibc}/lib/ld-linux${optionalString stdenv.is64bit "-x86-64"}.so.2 \
@@ -15,10 +9,7 @@ let
 
   udevRules = callPackage ./udev_rules_type1.nix {};
 
-in
-
-stdenv.mkDerivation rec {
-
+in stdenv.mkDerivation rec {
   name = "brscan4-0.4.3-3";
   src = fetchurl {
     url = "http://download.brother.com/welcome/dlf006645/${name}.amd64.deb";
@@ -32,8 +23,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ makeWrapper patchelf coreutils udevRules ];
   buildInputs = [ libusb ];
-  buildPhase = ":";
-
+  dontBuild = true;
 
   patchPhase = ''
     ${myPatchElf "opt/brother/scanner/brscan4/brsaneconfig4"}
@@ -47,7 +37,6 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-
     PATH_TO_BRSCAN4="opt/brother/scanner/brscan4"
     mkdir -p $out/$PATH_TO_BRSCAN4
     cp -rp $PATH_TO_BRSCAN4/* $out/$PATH_TO_BRSCAN4

pkgs/applications/misc/jbidwatcher/default.nix
@@ -15,9 +15,9 @@ stdenv.mkDerivation rec {
 
   jarfile = "$out/share/java/${pname}/JBidwatcher.jar";
 
-  unpackPhase = "true";
+#  unpackPhase = "true";
 
-  buildPhase = "true";
+  dontBuild = true;
 
   installPhase = ''
     mkdir -p "$out/bin"

pkgs/applications/misc/terminal-notifier/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
     sha256 = "09x7vl0kddivqq3pyrk6sg1f0sv5l7nj0bmblq222zk3b09bgg8p";
   };
 
-  buildPhase = "true";
+  dontBuild = true;
 
   installPhase = ''
     mkdir -p $out/Applications

pkgs/applications/version-management/cvs2svn/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [python makeWrapper];
 
-  buildPhase = "true";
+  dontBuild = true;
   installPhase = ''
     python ./setup.py install --prefix=$out
     for i in bzr svn git; do

pkgs/applications/version-management/git-and-tools/fast-export/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation {
 
   buildInputs = [mercurial.python mercurial makeWrapper subversion];
 
-  buildPhase="true"; # skip svn for now
+  dontBuild = true; # skip svn for now
 
   # TODO also support svn stuff
   # moving .py files into lib directory so that you can't pick the wrong file from PATH.

pkgs/applications/version-management/git-and-tools/svn2git/default.nix
@@ -13,22 +13,21 @@ stdenv.mkDerivation {
 
   buildInputs = [ ruby makeWrapper ];
 
-  buildPhase = "true";
+  dontBuild = true;
 
-  installPhase =
-    ''
-      mkdir -p $out
-      cp -r lib $out/
+  installPhase = ''
+    mkdir -p $out
+    cp -r lib $out/
 
-      mkdir -p $out/bin
-      substituteInPlace bin/svn2git --replace '/usr/bin/env ruby' ${ruby}/bin/ruby
-      cp bin/svn2git $out/bin/
-      chmod +x $out/bin/svn2git
+    mkdir -p $out/bin
+    substituteInPlace bin/svn2git --replace '/usr/bin/env ruby' ${ruby}/bin/ruby
+    cp bin/svn2git $out/bin/
+    chmod +x $out/bin/svn2git
 
-      wrapProgram $out/bin/svn2git \
-        --set RUBYLIB $out/lib \
-        --prefix PATH : ${git}/bin
-    '';
+    wrapProgram $out/bin/svn2git \
+      --set RUBYLIB $out/lib \
+      --prefix PATH : ${git}/bin
+  '';
 
   meta = {
     homepage = https://github.com/nirvdrum/svn2git;

commit cb1f37015ec229998dba4dc0ab63aaf5f723dd36

dosemu-fonts: Fix and simplify build
Regression introduced by f71cba2ff6c866ec819d1bd66472a9044db74512.

The commit only sets dontBuild but the build for the DOSEmu fonts had a
dummy configurePhase as well.

So rather than just reinserting a dummy configurePhase again, let's just
use buildCommand and put it out in one go, which is more suitable for
such a simple package.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

pkgs/data/fonts/dosemu-fonts/default.nix
@@ -1,31 +1,28 @@
-{stdenv, fetchurl, bdftopcf, mkfontdir, mkfontscale}:
+{ stdenv, fetchurl, bdftopcf, mkfontdir, mkfontscale }:
 
 stdenv.mkDerivation rec {
-  version = "1.4.0";
   name = "dosemu-fonts-${version}";
+  version = "1.4.0";
 
   src = fetchurl {
     url = "mirror://sourceforge/dosemu/dosemu-${version}.tgz";
     sha256 = "0l1zwmw42mpakjrzmbygshcg2qzq9mv8lx42738rz3j9hrqzg4pw";
   };
 
-  dontBuild = true;
-
-  buildInputs = [bdftopcf mkfontdir mkfontscale];
-
-  installPhase = ''
+  buildCommand = ''
+    tar xf "$src" --anchored --wildcards '*/etc/*.bdf' '*/etc/dosemu.alias'
     fontPath="$out/share/fonts/X11/misc/dosemu"
     mkdir -p "$fontPath"
-    for i in etc/*.bdf; do
+    for i in */etc/*.bdf; do
       fontOut="$out/share/fonts/X11/misc/dosemu/$(basename "$i" .bdf).pcf.gz"
       echo -n "Installing font $fontOut..." >&2
-      bdftopcf $i | gzip -c -9 > "$fontOut"
+      ${bdftopcf}/bin/bdftopcf $i | gzip -c -9 > "$fontOut"
       echo " done." >&2
     done
-    cp etc/dosemu.alias "$fontPath/fonts.alias"
+    cp */etc/dosemu.alias "$fontPath/fonts.alias"
     cd "$fontPath"
-    mkfontdir
-    mkfontscale
+    ${mkfontdir}/bin/mkfontdir
+    ${mkfontscale}/bin/mkfontscale
   '';
 
   meta = {