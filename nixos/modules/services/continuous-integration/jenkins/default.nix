jamagin/nixpkgs

commit 4435d56336ed8580787dd50e7095b7aa95d320f2

jenkins: move .war file from $out to $out/lib/jenkins.war
Fixes #14137, also known as:

  $ nix-shell -p jenkins
  bash: source: /nix/store/ln1yw6c2v8bb2cjqfr1z5aqcssw054wa-jenkins-2.3:
  cannot execute binary file
  [nix-shell exited with error]

The problem is that jenkins.war is not installed inside the directory
$out, but rather _as the file_ $out. Fix it by moving the file to
$out/lib/jenkins.war.

While at it, move buildCommand so that the "meta" section is at the end
of the expression (standard style), and quote shell variables.

nixos/modules/services/continuous-integration/jenkins/default.nix
@@ -154,7 +154,7 @@ in {
       '';
 
       script = ''
-        ${pkgs.jdk}/bin/java -jar ${pkgs.jenkins} --httpListenAddress=${cfg.listenAddress} \
+        ${pkgs.jdk}/bin/java -jar ${pkgs.jenkins}/lib/jenkins.war --httpListenAddress=${cfg.listenAddress} \
                                                   --httpPort=${toString cfg.port} \
                                                   --prefix=${cfg.prefix} \
                                                   ${concatStringsSep " " cfg.extraOptions}

pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -8,6 +8,12 @@ stdenv.mkDerivation rec {
     url = "http://mirrors.jenkins-ci.org/war/${version}/jenkins.war";
     sha256 = "0x59dbvh6y25ki5jy51djbfbhf8g2j3yd9f3n66f7bkdfw8p78g1";
   };
+
+  buildCommand = ''
+    mkdir -p "$out/lib"
+    cp "$src" "$out/lib/jenkins.war"
+  '';
+
   meta = with stdenv.lib; {
     description = "An extendable open source continuous integration server";
     homepage = http://jenkins-ci.org;
@@ -15,6 +21,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.all;
     maintainers = [ maintainers.coconnor ];
   };
-
-  buildCommand = "cp $src $out";
 }

commit 9a8064d1f95f2e2d93c362f46bdd3a1f1ab20bbb

jenkins: move $out/{lib => webapps}/jenkins.war
As pointed out by @danbst, the tomcat NixOS module expects packages
listed in services.tomcat.webapps to either be direct .war file paths or
have .war files inside a "webapps" directory.

Commit 4435d56336ed8580787dd50e7095b7aa95d320f2
("jenkins: move .war file from $out to $out/lib/jenkins.war") broke
jenkins + tomcat. Fix it by moving jenkins.war to $out/webapps/.

nixos/modules/services/continuous-integration/jenkins/default.nix
@@ -154,7 +154,7 @@ in {
       '';
 
       script = ''
-        ${pkgs.jdk}/bin/java -jar ${pkgs.jenkins}/lib/jenkins.war --httpListenAddress=${cfg.listenAddress} \
+        ${pkgs.jdk}/bin/java -jar ${pkgs.jenkins}/webapps/jenkins.war --httpListenAddress=${cfg.listenAddress} \
                                                   --httpPort=${toString cfg.port} \
                                                   --prefix=${cfg.prefix} \
                                                   ${concatStringsSep " " cfg.extraOptions}

pkgs/development/tools/continuous-integration/jenkins/default.nix
@@ -10,8 +10,8 @@ stdenv.mkDerivation rec {
   };
 
   buildCommand = ''
-    mkdir -p "$out/lib"
-    cp "$src" "$out/lib/jenkins.war"
+    mkdir -p "$out/webapps"
+    cp "$src" "$out/webapps/jenkins.war"
   '';
 
   meta = with stdenv.lib; {