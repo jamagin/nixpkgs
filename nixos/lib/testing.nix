jamagin/nixpkgs

commit 538e5b7eca55751226f6e0c654d944d643937ab0

runInMachine: Unset another variable
Thanks @cstrahan.

nixos/lib/testing.nix
@@ -181,7 +181,7 @@ rec {
           eval $i2=/tmp/xchg/$_basename
           ${coreutils}/bin/ls -la $xchg
         done
-        unset i i2 _basename
+        unset i i2 _basename xchg
 
         export > $xchg/saved-env
         export tests='${testScript}'

commit 67da07e6936bb4d95f62153826839aa0fb7441ad

nixos/lib/testing: Fix unsetting $xchg
Regression introduced by 538e5b7eca55751226f6e0c654d944d643937ab0.

Unsetting the environment variable shortly before using it is not going
to end up very well, so let's just filter out the variable from the
output of export and unset it shortly afterwards.

This fixes the runInMachine NixOS test.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

nixos/lib/testing.nix
@@ -181,9 +181,11 @@ rec {
           eval $i2=/tmp/xchg/$_basename
           ${coreutils}/bin/ls -la $xchg
         done
-        unset i i2 _basename xchg
 
-        export > $xchg/saved-env
+        unset i i2 _basename
+        export | ${gnugrep}/bin/grep -v '^xchg=' > $xchg/saved-env
+        unset xchg
+
         export tests='${testScript}'
         ${testDriver}/bin/nixos-test-driver ${vm.config.system.build.vm}/bin/run-*-vm
       ''; # */