jamagin/nixpkgs

commit da22f378737659a08f9bd1adf1b13f7e72f7e112

ccache: support clang in ccache.links

pkgs/development/tools/misc/ccache/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, fetchpatch, runCommand, gcc, zlib }:
+{ stdenv, fetchurl, fetchpatch, runCommand, zlib }:
 
 let ccache = stdenv.mkDerivation rec {
   name = "ccache-${version}";
@@ -17,41 +17,44 @@ let ccache = stdenv.mkDerivation rec {
 
   doCheck = !stdenv.isDarwin;
 
-  passthru = {
+  passthru = let
+      cc = stdenv.cc.cc;
+      ccname = if stdenv.cc.cc.isClang or false then "clang" else "gcc";
+      cxxname = if stdenv.cc.cc.isClang or false then "clang++" else "g++";
+    in {
     # A derivation that provides gcc and g++ commands, but that
     # will end up calling ccache for the given cacheDir
     links = extraConfig: stdenv.mkDerivation rec {
       name = "ccache-links";
       passthru = {
-        inherit gcc;
-        isGNU = true;
+        inherit (cc) isGNU isClang;
       };
-      inherit (gcc.cc) lib;
+      inherit (cc) lib;
       buildCommand = ''
         mkdir -p $out/bin
-        if [ -x "${gcc.cc}/bin/gcc" ]; then
-          cat > $out/bin/gcc << EOF
+        if [ -x "${cc}/bin/${ccname}" ]; then
+          cat > $out/bin/${ccname} << EOF
           #!/bin/sh
           ${extraConfig}
-          exec ${ccache}/bin/ccache ${gcc.cc}/bin/gcc "\$@"
+          exec ${ccache}/bin/ccache ${cc}/bin/${ccname} "\$@"
         EOF
-          chmod +x $out/bin/gcc
+          chmod +x $out/bin/${ccname}
         fi
-        if [ -x "${gcc.cc}/bin/g++" ]; then
-          cat > $out/bin/g++ << EOF
+        if [ -x "${cc}/bin/${cxxname}" ]; then
+          cat > $out/bin/${cxxname} << EOF
           #!/bin/sh
           ${extraConfig}
-          exec ${ccache}/bin/ccache ${gcc.cc}/bin/g++ "\$@"
+          exec ${ccache}/bin/ccache ${cc}/bin/${cxxname} "\$@"
         EOF
-          chmod +x $out/bin/g++
+          chmod +x $out/bin/${cxxname}
         fi
-        for executable in $(ls ${gcc.cc}/bin); do
+        for executable in $(ls ${cc}/bin); do
           if [ ! -x "$out/bin/$executable" ]; then
-            ln -s ${gcc.cc}/bin/$executable $out/bin/$executable
+            ln -s ${cc}/bin/$executable $out/bin/$executable
           fi
         done
-        for file in $(ls ${gcc.cc} | grep -vw bin); do
-          ln -s ${gcc.cc}/$file $out/$file
+        for file in $(ls ${cc} | grep -vw bin); do
+          ln -s ${cc}/$file $out/$file
         done
       '';
     };

commit cca24cd70f8e538d719288bda2a51ae9758d19a6

ccache: improve compiler wrapper
* move compiler detection logic from nix to bash
* robust inheritance of isGNU/isClang

Fixes: da22f37873 ('ccache: support clang in ccache.links')
Fixes: cdbb601d84 ('ccache: fix references to stdenv.cc.cc.isClang')

pkgs/development/tools/misc/ccache/default.nix
@@ -18,44 +18,46 @@ let ccache = stdenv.mkDerivation rec {
   doCheck = !stdenv.isDarwin;
 
   passthru = let
-      cc = stdenv.cc.cc;
-      ccname = if stdenv.cc.isClang then "clang" else "gcc";
-      cxxname = if stdenv.cc.isClang then "clang++" else "g++";
+      unwrappedCC = stdenv.cc.cc;
     in {
     # A derivation that provides gcc and g++ commands, but that
     # will end up calling ccache for the given cacheDir
     links = extraConfig: stdenv.mkDerivation rec {
       name = "ccache-links";
       passthru = {
-        inherit (stdenv.cc) isClang;
-        inherit (cc) isGNU;
+        isClang = unwrappedCC.isClang or false;
+        isGNU = unwrappedCC.isGNU or false;
       };
-      inherit (cc) lib;
+      inherit (unwrappedCC) lib;
       buildCommand = ''
         mkdir -p $out/bin
-        if [ -x "${cc}/bin/${ccname}" ]; then
-          cat > $out/bin/${ccname} << EOF
-          #!/bin/sh
-          ${extraConfig}
-          exec ${ccache}/bin/ccache ${cc}/bin/${ccname} "\$@"
+
+        wrap() {
+          local cname="$1"
+          if [ -x "${unwrappedCC}/bin/$cname" ]; then
+            cat > $out/bin/$cname << EOF
+        #!/bin/sh
+        ${extraConfig}
+        exec ${ccache}/bin/ccache ${unwrappedCC}/bin/$cname "\$@"
         EOF
-          chmod +x $out/bin/${ccname}
-        fi
-        if [ -x "${cc}/bin/${cxxname}" ]; then
-          cat > $out/bin/${cxxname} << EOF
-          #!/bin/sh
-          ${extraConfig}
-          exec ${ccache}/bin/ccache ${cc}/bin/${cxxname} "\$@"
-        EOF
-          chmod +x $out/bin/${cxxname}
-        fi
-        for executable in $(ls ${cc}/bin); do
+            chmod +x $out/bin/$cname
+          fi
+        }
+
+        wrap cc
+        wrap c++
+        wrap gcc
+        wrap g++
+        wrap clang
+        wrap clang++
+
+        for executable in $(ls ${unwrappedCC}/bin); do
           if [ ! -x "$out/bin/$executable" ]; then
-            ln -s ${cc}/bin/$executable $out/bin/$executable
+            ln -s ${unwrappedCC}/bin/$executable $out/bin/$executable
           fi
         done
-        for file in $(ls ${cc} | grep -vw bin); do
-          ln -s ${cc}/$file $out/$file
+        for file in $(ls ${unwrappedCC} | grep -vw bin); do
+          ln -s ${unwrappedCC}/$file $out/$file
         done
       '';
     };