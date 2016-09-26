jamagin/nixpkgs

commit ea5e9988e3d53dbe08e33dc8865ac7de63fd5334

getmail: 4.50.0 -> 4.51.0

pkgs/tools/networking/getmail/default.nix
@@ -1,13 +1,13 @@
-{ stdenv, fetchurl, pythonPackages }:
+{ stdenv, fetchurl, buildPythonApplication }:
 
-pythonPackages.buildPythonApplication rec {
-  version = "4.50.0";
+buildPythonApplication rec {
+  version = "4.51.0";
   name = "getmail-${version}";
   namePrefix = "";
 
   src = fetchurl {
     url = "http://pyropus.ca/software/getmail/old-versions/${name}.tar.gz";
-    sha256 = "1hcb5079mkcx3gglfycrhglrgg4jsa499br50yjrh9sal6wpgg7w";
+    sha256 = "1nzdjm62lrg7qbh8xz2irj9wxf2861s3ld6zd5fbcgpjy3v588mw";
   };
 
   doCheck = false;

commit 91663ad9e9df48faf42030df8f7abbe2b8e1b9b3

getmail: pass in pythonPackages again
broken by ea5e9988e3d53dbe08e33dc8865ac7de63fd5334

pkgs/tools/networking/getmail/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchurl, buildPythonApplication }:
+{ stdenv, fetchurl, pythonPackages }:
 
-buildPythonApplication rec {
+pythonPackages.buildPythonApplication rec {
   version = "4.51.0";
   name = "getmail-${version}";
   namePrefix = "";