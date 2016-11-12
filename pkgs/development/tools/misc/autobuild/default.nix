jamagin/nixpkgs

commit 2ce748bdbf11f10aca80bbec1cbd7360cbe218f6

autobuild: 3.5 -> 5.3

pkgs/development/tools/misc/autobuild/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, stdenv, makeWrapper, perl, openssh, rsync }:
 
 stdenv.mkDerivation rec {
-  name = "autobuild-3.5";
+  name = "autobuild-5.3";
 
   src = fetchurl {
     url = "http://savannah.spinellicreations.com/autobuild/${name}.tar.gz";
-    sha256 = "0ik13913x1yj8lsaf65chpiw13brl3q6kx7s1fa41a7s2krl1xvi";
+    sha256 = "0gv7g61ja9q9zg1m30k4snqwwy1kq7b4df6sb7d2qra7kbdq8af1";
   };
 
   buildInputs = [ makeWrapper perl openssh rsync ];

commit 59a53be91de0c2a1c9083cf4c02dc910275a1f1d

fix autobuild compilation
Currently autobuild failed to build in both master and release-16.03. The
error message is

   mv: cannot stat '/nix/store/a4jzlkrhd4b4nr5760caj3rrjxhyg4z3-autobuild-5.3/bin/abput-sourceforge': No such file or directory
   builder for ‘/nix/store/l19vywq8zvbarswby5h7vl4iq1c1pvq7-autobuild-5.3.drv’ failed with exit code 1

It happens in post install script

   postInstall = ''
     wrapProgram $out/bin/ab{put,build}-sourceforge \
       --prefix PATH ":" "${stdenv.lib.makeBinPath [ openssh rsync ]}"
   '';

The problem is that in autobuild 5.0 the ab{put,build}-sourceforge were removed

   * Version 5.0 (released 2008-11-25)
   ...
   ** abput, abput-sourceforge, abput-testdrive: Remove.
   ** abbuild, abbuild-sourceforge: Remove.
   Both the SourceForge and TestDrive systems have been shutdown.  The
   scripts were of questionable value anyway.

Possibly the bug was introduced in commit
2ce748bdbf11f10aca80bbec1cbd7360cbe218f6: autobuild: 3.5 -> 5.3.

As the package doesn't contain these programs there is no need in wrapping
them. This commit just remove the postInstall section of corresponding
package.

pkgs/development/tools/misc/autobuild/default.nix
@@ -12,11 +12,6 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
 
-  postInstall = ''
-    wrapProgram $out/bin/ab{put,build}-sourceforge \
-      --prefix PATH ":" "${stdenv.lib.makeBinPath [ openssh rsync ]}"
-  '';
-
   meta = {
     description = "Continuous integration tool";
 