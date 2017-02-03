jamagin/nixpkgs

commit ebfae8fbb66065c8ee074d482379b50d5e83c2e8

linuxPackages.perf: fix build with gcc6

pkgs/os-specific/linux/kernel/perf.nix
@@ -30,7 +30,10 @@ stdenv.mkDerivation {
 
   # Note: we don't add elfutils to buildInputs, since it provides a
   # bad `ld' and other stuff.
-  NIX_CFLAGS_COMPILE = "-Wno-error=cpp -Wno-error=bool-compare -Wno-error=deprecated-declarations";
+  NIX_CFLAGS_COMPILE = [
+    "-Wno-error=cpp" "-Wno-error=bool-compare" "-Wno-error=deprecated-declarations"
+    "-Wno-error=unused-const-variable" "-Wno-error=misleading-indentation"
+  ];
 
   installFlags = "install install-man ASCIIDOC8=1";
 

commit dfab89ba73a2ea5aab703f9ca8fdc567dd44b0b3

linuxPackages*.perf: fix build with default gcc
Broken since ebfae8fb.

pkgs/os-specific/linux/kernel/perf.nix
@@ -32,8 +32,11 @@ stdenv.mkDerivation {
   # bad `ld' and other stuff.
   NIX_CFLAGS_COMPILE = [
     "-Wno-error=cpp" "-Wno-error=bool-compare" "-Wno-error=deprecated-declarations"
-    "-Wno-error=unused-const-variable" "-Wno-error=misleading-indentation"
-  ];
+  ]
+    # gcc before 6 doesn't know these options
+    ++ stdenv.lib.optionals (hasPrefix "gcc-6" stdenv.cc.cc) [
+      "-Wno-error=unused-const-variable" "-Wno-error=misleading-indentation"
+    ];
 
   installFlags = "install install-man ASCIIDOC8=1";
 