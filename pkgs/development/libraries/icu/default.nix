jamagin/nixpkgs

commit 192b6c8ab8b2d3305350d27a97182ebcb82970ef

icu: Trivial improvement

pkgs/development/libraries/icu/default.nix
@@ -33,18 +33,9 @@ stdenv.mkDerivation ({
     echo Source root reset to ''${sourceRoot}
   '';
 
-  # This pre/postPatch shenanigans is to handle that the patches expect
-  # to be outside of `source`.
-  prePatch = ''
-    pushd ..
-  '';
-  postPatch = ''
-    popd
-    patch -p4 < ${keywordFix}
-  '';
+  patchFlags = "-p4";
 
-  patches = [
-  ];
+  patches = [ keywordFix ];
 
   preConfigure = ''
     sed -i -e "s|/bin/sh|${stdenv.shell}|" configure

commit d1aa90f89b5176dd14f7fededc607adfc0ef3ee2

icu: Fix ARM patch
Broken by 192b6c8ab8b2d3305350d27a97182ebcb82970ef.
Let's do it in a cleaner way.

pkgs/development/libraries/icu/0001-Disable-LDFLAGSICUDT-for-Linux.patch
@@ -1,28 +1,0 @@
-From 0c82d6aa02c08e41b13c83b14782bd7024e25d59 Mon Sep 17 00:00:00 2001
-From: Khem Raj <raj.khem@gmail.com>
-Date: Sat, 15 Feb 2014 21:06:42 +0000
-Subject: [PATCH] Disable LDFLAGSICUDT for Linux
-
-Upstream-Status: Inappropriate [ OE Configuration ]
-
-Signed-off-by: Khem Raj <raj.khem@gmail.com>
----
- source/config/mh-linux |    2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/source/config/mh-linux b/source/config/mh-linux
-index 366f0cc..2689aab 100644
---- a/source/config/mh-linux
-+++ b/source/config/mh-linux
-@@ -21,7 +21,7 @@ LD_RPATH= -Wl,-zorigin,-rpath,'$$'ORIGIN
- LD_RPATH_PRE = -Wl,-rpath,
- 
- ## These are the library specific LDFLAGS
--LDFLAGSICUDT=-nodefaultlibs -nostdlib
-+# LDFLAGSICUDT=-nodefaultlibs -nostdlib
- 
- ## Compiler switch to embed a library name
- # The initial tab in the next line is to prevent icu-config from reading it.
--- 
-1.7.10.4
-

pkgs/development/libraries/icu/default.nix
@@ -12,7 +12,7 @@ let
     sha256 = "0hxhpgydalyxacaaxlmaddc1sjwh65rsnpmg0j414mnblq74vmm8";
   };
 in
-stdenv.mkDerivation ({
+stdenv.mkDerivation {
   name = pname + "-" + version;
 
   src = fetchurl {
@@ -39,6 +39,9 @@ stdenv.mkDerivation ({
 
   preConfigure = ''
     sed -i -e "s|/bin/sh|${stdenv.shell}|" configure
+  '' + stdenv.lib.optionalString stdenv.isArm ''
+    # From https://archlinuxarm.org/packages/armv7h/icu/files/icudata-stdlibs.patch
+    sed -e 's/LDFLAGSICUDT=-nodefaultlibs -nostdlib/LDFLAGSICUDT=/' -i config/mh-linux
   '';
 
   configureFlags = "--disable-debug" +
@@ -59,6 +62,4 @@ stdenv.mkDerivation ({
     maintainers = with maintainers; [ raskin ];
     platforms = platforms.all;
   };
-} // (if stdenv.isArm then {
-  patches = [ ./0001-Disable-LDFLAGSICUDT-for-Linux.patch ];
-} else {}))
+}