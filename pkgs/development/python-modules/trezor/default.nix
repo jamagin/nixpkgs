jamagin/nixpkgs

commit 94273cf4476e9f44a5433bfa5b2f98c02846d37c

python.pkgs: several maintenance bumps

pkgs/development/python-modules/trezor/default.nix
@@ -3,11 +3,11 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "trezor";
-  version = "0.7.13";
+  version = "0.7.15";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d05f388bb56b6f61cc727999cc725078575238a0b6172450322bc55c437fefe5";
+    sha256 = "f7e4f509263ca172532b4c0a440d164add7cdc021b4370a253d51eba5806b618";
   };
 
   propagatedBuildInputs = [ protobuf3_0 hidapi ];

pkgs/development/python-modules/Nikola/default.nix
@@ -30,7 +30,7 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "Nikola";
-  version = "7.8.7";
+  version = "7.8.8";
 
   # Nix contains only Python 3 supported version of doit, which is a dependency
   # of Nikola. Python 2 support would require older doit 0.29.0 (which on the
@@ -47,7 +47,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "d9c77ce9758cc0e848d4c99229a28314e8bd2a590c77c56540fa919fca4d779f";
+    sha256 = "a45272e96ea8311e79ab6ea5523d792207d549cba418d623f03bf80d5e5e3e5a";
   };
 
   meta = {

pkgs/development/python-modules/aenum/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "aenum";
-  version = "2.0.7";
+  version = "2.0.8";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2c5db863b5531cc059313018e57bc765b0ef1fc96ba799f105ea45d99b1c2d23";
+    sha256 = "3209fa41b8c41345442e8d9b5158a57d3e96d84c3d5ebbe8e521e1e2eff1598d";
   };
 
   doCheck = !isPy3k;

pkgs/development/python-modules/asgi_redis/default.nix
@@ -2,13 +2,13 @@
   asgiref, asgi_ipc, msgpack, six, redis, cryptography
 }:
 buildPythonPackage rec {
-  version = "1.4.0";
+  version = "1.4.1";
   pname = "asgi_redis";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://pypi/a/asgi_redis/${name}.tar.gz";
-    sha256 = "ec137829a9ebfb0de1c034bc699240c9747b97a3eb2dc4df6c812f82290a0f9f";
+    sha256 = "71a6147c8b08833815993f16e2b665a0e5483fae392f03decb8da54ed0882904";
   };
 
   # Requires a redis server available

pkgs/development/python-modules/channels/default.nix
@@ -4,11 +4,11 @@
 buildPythonPackage rec {
   pname = "channels";
   name = "${pname}-${version}";
-  version = "1.1.3";
+  version = "1.1.5";
 
   src = fetchurl {
     url = "mirror://pypi/c/channels/${name}.tar.gz";
-    sha256 = "182war437i6wsxwf2v4szn8ig0nkpinpn4n27fxhh5q8w832hj93";
+    sha256 = "a9005bcb6104d26a7f93d9cf012bcf6765a0ff444a449ac68d6e1f16721f8ed3";
   };
 
   # Files are missing in the distribution

pkgs/development/python-modules/edward/default.nix
@@ -3,14 +3,14 @@
 
 buildPythonPackage rec {
   pname = "edward";
-  version = "1.3.2";
+  version = "1.3.3";
   name  = "${pname}-${version}";
 
   disabled = !(isPy27 || pythonAtLeast "3.4");
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "24aa0bf237965f47dd39d2b2ff43718ba75cb12c471b26ff80a972a66ff32de3";
+    sha256 = "104d58321c5040235b3039ae3215c3c7881073e6aa88bb0b8ca1141ca87c4891";
   };
 
   # disabled for now due to Tensorflow trying to create files in $HOME:

pkgs/development/python-modules/fonttools/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "fonttools";
-  version = "3.13.0";
+  version = "3.13.1";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "5ec278ff231d0c88afe8266e911ee0f8e66c8501c53f5f144a1a0abbc936c6b8";
+    sha256 = "ded1f9a6cdd6ed19a3df05ae40066d579ffded17369b976f9e701cf31b7b1f2d";
     extension = "zip";
   };
 

pkgs/development/python-modules/keras/default.nix
@@ -12,12 +12,12 @@
 
 buildPythonPackage rec {
   pname = "Keras";
-  version = "2.0.4";
+  version = "2.0.5";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1cbe62af6821963321b275d5598fd94e63c11feaa1d4deaa79c9eb9ee0e1d68a";
+    sha256 = "cbce24758530e070fe1b403d6d21391cbea78c037b70bf6afc1ca9f1f8269eff";
   };
 
   checkInputs = [

commit dcd61d74639481f383ceea01f6df6efb92368869

python-modules/trezor: Fix build
Regression introduced by 94273cf4476e9f44a5433bfa5b2f98c02846d37c.

With version 0.7.15 a few additional dependencies are needed by trezor,
mainly a newer version of protobuf bindings and requests.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Cc: @np

pkgs/development/python-modules/trezor/default.nix
@@ -1,4 +1,6 @@
-{ lib, fetchPypi, buildPythonPackage, protobuf3_0, hidapi, ecdsa, mnemonic }:
+{ lib, fetchPypi, buildPythonPackage, protobuf3_2, hidapi, ecdsa, mnemonic
+, requests
+}:
 
 buildPythonPackage rec {
   name = "${pname}-${version}";
@@ -10,7 +12,7 @@ buildPythonPackage rec {
     sha256 = "f7e4f509263ca172532b4c0a440d164add7cdc021b4370a253d51eba5806b618";
   };
 
-  propagatedBuildInputs = [ protobuf3_0 hidapi ];
+  propagatedBuildInputs = [ protobuf3_2 hidapi requests ];
 
   buildInputs = [ ecdsa mnemonic ];
 