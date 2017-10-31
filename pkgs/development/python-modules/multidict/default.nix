jamagin/nixpkgs

commit 05a2fe5d556c611c3a89f7614a16b6519fe3a459

python.pkgs: updates

pkgs/development/python-modules/multidict/default.nix
@@ -8,13 +8,13 @@
 
 let
   pname = "multidict";
-  version = "3.1.3";
+  version = "3.3.0";
 in buildPythonPackage rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
-    sha256 = "875f80a046e7799b40df4b015b8fc5dae91697936872a8d7362c909a02ec6d12";
+    sha256 = "e76909da2fad6966281d4e0e4ccfd3c3025699ebcc30806afa09fa1384c3532b";
   };
 
   checkInputs = [ pytest psutil ];

pkgs/development/python-modules/APScheduler/default.nix
@@ -20,12 +20,12 @@
 
 buildPythonPackage rec {
   pname = "APScheduler";
-  version = "3.3.1";
+  version = "3.4.0";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f68874dff1bdffcc6ce3adb7840c1e4d162c609a3e3f831351df30b75732767b";
+    sha256 = "b51118a8ed014104f7e440456dcbd90f2015aea7bcc34c57e307fb34bc746316";
   };
 
   buildInputs = [

pkgs/development/python-modules/Cython/default.nix
@@ -14,11 +14,11 @@
 buildPythonPackage rec {
   pname = "Cython";
   name = "${pname}-${version}";
-  version = "0.26.1";
+  version = "0.27.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "c2e63c4794161135adafa8aa4a855d6068073f421c83ffacc39369497a189dd5";
+    sha256 = "265dacf64ed8c0819f4be9355c39beaa13dc2ad2f85237a2c4e478f5ce644b48";
   };
 
   # With Python 2.x on i686-linux or 32-bit ARM this test fails because the

pkgs/development/python-modules/MechanicalSoup/default.nix
@@ -1,18 +1,30 @@
 { fetchPypi, buildPythonPackage, lib
-, requests, beautifulsoup4, six }:
+, requests, beautifulsoup4, six
+, pytestrunner, requests-mock, pytestcov, pytest
+}:
 
 
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "MechanicalSoup";
-  version = "0.7.0";
+  version = "0.8.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0wh93rml446ipx603n5z5i5bpan46pzliq6sw76d0ms9w7w2658d";
+    sha256 = "38a6ca35428196be94f87f8f036ee4a88b1418d1f77e5634ad92acfaa22c28da";
   };
 
+  checkInputs = [ pytest pytestrunner requests-mock pytestcov ];
+
   propagatedBuildInputs = [ requests beautifulsoup4 six ];
+
+  # Requires network
+  doCheck = false;
+
+  postPatch = ''
+    # Is in setup_requires but not used in setup.py...
+    substituteInPlace setup.py --replace "'pytest-runner'," ""
+  '';
 
   meta = with lib; {
     description = "A Python library for automating interaction with websites";

pkgs/development/python-modules/Nikola/default.nix
@@ -30,7 +30,7 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "Nikola";
-  version = "7.8.9";
+  version = "7.8.10";
 
   # Nix contains only Python 3 supported version of doit, which is a dependency
   # of Nikola. Python 2 support would require older doit 0.29.0 (which on the
@@ -47,7 +47,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "c85bf293a245a34057fb55236fc2f2c5d28e9ef1c375889f443bca1d86924df4";
+    sha256 = "e242c3d0dd175d95a0baf5268b108081ba160b83ceafec8c9bc2ec0a24a56537";
   };
 
   meta = {

pkgs/development/python-modules/aiofiles/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "aiofiles";
-  version = "0.3.1";
+  version = "0.3.2";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "6c4936cea65175277183553dbc27d08b286a24ae5bd86f44fbe485dfcf77a14a";
+    sha256 = "852a493a877b73e11823bfd4e8e5ef2610d70d12c9eaed961bcd9124d8de8c10";
   };
 
   disabled = pythonOlder "3.3";

pkgs/development/python-modules/aiohttp/default.nix
@@ -13,12 +13,12 @@
 
 buildPythonPackage rec {
   pname = "aiohttp";
-  version = "2.2.5";
+  version = "2.3.1";
   name = "${pname}-${version}";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "af5bfdd164256118a0a306b3f7046e63207d1f8cba73a67dcc0bd858dcfcd3bc";
+    sha256 = "04f58bbcc9ae6f9aec30b9219ae47fa3c31586c77679405720545738ea62236e";
   };
 
   disabled = pythonOlder "3.4";

pkgs/development/python-modules/alembic/default.nix
@@ -6,11 +6,11 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "alembic";
-  version = "0.9.5";
+  version = "0.9.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "8bdcb4babaa16b9a826f8084949cc2665cb328ecf7b89b3224b0ab85bd16fd05";
+    sha256 = "042851ebe9efa07be6dc1395b1793b6c1d8964a39b73a0ce1649e2bcd41ea732";
   };
 
   buildInputs = [ pytest pytestcov mock coverage ];

commit 095cc3e0ca2372c3220be6f5cdb69928fda579a4

pythonPackages.multidict: Fix the build and tests
(Was broken since 05a2fe5d556c611c3a89f7614a16b6519fe3a459)

pkgs/development/python-modules/multidict/default.nix
@@ -1,9 +1,9 @@
 { lib
 , fetchurl
 , buildPythonPackage
-, pytest
+, cython
+, pytest, psutil, pytestrunner
 , isPy3k
-, psutil
 }:
 
 let
@@ -17,11 +17,8 @@ in buildPythonPackage rec {
     sha256 = "e76909da2fad6966281d4e0e4ccfd3c3025699ebcc30806afa09fa1384c3532b";
   };
 
-  checkInputs = [ pytest psutil ];
-
-  checkPhase = ''
-    py.test
-  '';
+  buildInputs = [ cython ];
+  checkInputs = [ pytest psutil pytestrunner ];
 
   disabled = !isPy3k;
 